0847da2d36b1f3530882c6bbca676ca73a621fa5
hara@laptop <harunhebibovic05@gmail.com>
2025-02-27 23:27:56 +0100

    created and imported components

--- redux-tutorial/src/App.jsx
+++ redux-tutorial/src/App.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import CarForm from "./components/CarForm";
+import CarList from "./components/CarList";
+import CarSearch from "./components/CarSearch";
+import CarValue from "./components/CarValue";
 
 const App = () => {
   return (
     <div>
-      <h1> 2nd part </h1>
+      <CarForm />
+      <CarList />
+      <CarSearch />
+      <CarValue />
     </div>
   );
 };
